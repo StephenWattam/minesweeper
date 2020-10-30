96a107b873be3c54fc4e2bc9ba489a8b4351f30e
Steve Wattam <steve@stephenwattam.com>
2020-10-30 01:28:14 +0000

    Non-deterministic AI paired with deterministic AI

--- ai.py
+++ ai.py
@@ -4,10 +4,16 @@ FLAGGED = "f"
 
 class MineSweeperAI:
 
-    def __init__(self, game, board):
+    def __init__(self, game, board, board_density_threshold=0.2):
 
         self.game  = game
         self.board = board
+
+        # Behaviour params
+
+        # After seeing this proportion of the board, a routine will be run to estimate the probability
+        # of seeing a mine in an unknown square
+        self.board_density_threshold = board_density_threshold
 
     @staticmethod
     def _write_with_limit(obj, x, y, val):
@@ -40,16 +46,15 @@ class MineSweeperAI:
 
         return count, which
 
-    def move(self):
-
-        # Things we know about the board, p[bomb]
-        knowledge = [[None] * self.board.width() for _ in range(self.board.height())]
+    def _read_observable_state(self):
+        """Get observable state and basic knowledge of the board"""
 
         # Precompute our view of the board, censored according to revealed status
         cells = [[self._observable_state(x, y) for x in range(self.board.width())] for y in range(self.board.height())]
 
+        # Things we know about the board, p[bomb]
+        knowledge = [[None] * self.board.width() for _ in range(self.board.height())]
 
-        # PASS 1:
         # known facts about 0 cells
         for y in range(len(cells)):
             for x in range(len(cells[y])):
@@ -61,7 +66,7 @@ class MineSweeperAI:
                 # Unknown cell --- not informative
                 if state is None:
                     continue
-                # Flagged cell --- not informative
+                # Flagged cell --- we know it's not a bome
                 if state is FLAGGED:
                     knowledge[y][x] = 1
                     continue
@@ -80,48 +85,187 @@ class MineSweeperAI:
                     MineSweeperAI._write_with_limit(knowledge, x,   y+1, 0)
                     MineSweeperAI._write_with_limit(knowledge, x+1, y+1, 0)
 
-        # PASS 2
-        # If a number has a certain number of unknowns, all are bombs
+        return cells, knowledge
+
+    def _move_determinstic(self):
+        """Deduce the moves that are guaranteed to be correct due to the hints given by
+        cell numbers.
+
+        Returns only when all actions that could be taken have been taken"""
+
+        # Have we done anything to change the board this tick?
+        action_this_loop = True
+        # Have we done anything at all this method?
+        action = False
+        while action_this_loop:
+
+            cells, knowledge = self._read_observable_state()
+            action_this_loop = False
+
+            # PASS 1
+            # If a number has a certain number of unknowns, all are bombs and should be set as such
+            for y in range(len(cells)):
+                for x in range(len(cells[y])):
+
+                    state = cells[y][x]
+
+                    # Unknown cell: skip
+                    if state is None or state == FLAGGED:
+                        continue
+
+                    if state > 0:
+                        # Count adjacent unknowns
+                        adjacent_unknown_count, coords = MineSweeperAI._count_adjacent(knowledge, x, y, lambda x: x is None or x == 1)
+
+                        # If we have exactly the number of unknowns as cell number, set all to flags.
+                        # Because setting a cell as flagged in the game only affects a single cell we can keep
+                        # track of the knowledge changes by dead reckoning, no need to re-read the board
+                        if adjacent_unknown_count == state:
+                            for target_x, target_y in coords:
+                                if not self.game.cell_flagged(target_x, target_y):
+                                    self.game.toggle_flag(target_x, target_y)
+                                    action_this_loop = True
+                                    knowledge[target_y][target_x] = 1
+
+
+            # PASS 2
+            # After flagging, if something has only unknowns left, all are not bombs
+            for y in range(len(cells)):
+                for x in range(len(cells[y])):
+
+                    state = cells[y][x]
+
+                    # Unknown cell: skip
+                    if state is None or state == FLAGGED:
+                        continue
+
+                    if state > 0:
+                        # Count adjacent unknowns
+                        adjacent_flag_count, _ = MineSweeperAI._count_adjacent(knowledge, x, y, lambda x: x == 1)
+                        adjacent_unknown_count, coords = MineSweeperAI._count_adjacent(knowledge, x, y, lambda x: x is None)
+
+                        # Click to reveal those cells that are around these numbers but known to not be
+                        # bombs
+                        #
+                        # After clicking we have to re-read the board because the click could have
+                        # revealed a large area of board
+                        if adjacent_flag_count == state and adjacent_unknown_count > 0:
+                            for target_x, target_y in coords:
+                                self.game.click(target_x, target_y)
+                                action_this_loop = True
+                                knowledge[target_y][target_x] = 0
+
+            # Keep track for return of method
+            action = action_this_loop or action
+
+        # Say if we did anything during this method
+        return action
+
+
+    def _move_heuristic(self):
+        """Look at unknown cells and score them for how likely they are to be mines.
+
+        Select the least likely move.
+
+        Returns after a single action."""
+
+        cells, knowledge = self._read_observable_state()
+
+        # Compute basic probability there is a mine left in any cell.
+        # Do this by looking at what we know about the board, and assuming roughly uniform distribution,
+        # that is, we known p[mine] by observing existing flags.
+        #
+        # Only do this if we have seen a certain area of the board
+        #
+        # FIXME: This is imperfect because cells are not entirely independent.
+        num_flags = 0
+        num_unknown = 0
+        baseline_probability = 0
+        for y in range(len(cells)):
+            for x in range(len(cells[y])):
+                if cells[y][x] is None:
+                    num_unknown += 1
+                elif cells[y][x] is FLAGGED:
+                    num_unknown += 1
+        if num_unknown > 0 and self.board.width() * self.board.height() / (num_unknown + num_flags):
+            baseline_probability = num_flags / num_unknown
+
+        # PASS 1
+        # If a number has a certain number of unknowns, all are bombs and should be set as such
+        unknown_cell_penalty = {}
         for y in range(len(cells)):
             for x in range(len(cells[y])):
 
                 state = cells[y][x]
 
-                # Unknown cell: skip
-                if state is None or state == FLAGGED:
+                # Look at unknowns only
+                if state is not None:
                     continue
 
-                if state > 0:
-                    # Count adjacent unknowns
-                    adjacent_unknown_count, coords = MineSweeperAI._count_adjacent(knowledge, x, y, lambda x: x is None or x == 1)
+                # Cell with adjacent unknown.  We want to discount the number of known mines
+                # around this cell, then penalise the unknown cells by however many mines are
+                # left indicated.  The sum of these overlaps will be proportional to the probability
+                # that the unknown cell is mined
+                adjacent_numbered_count, coords = MineSweeperAI._count_adjacent(cells, x, y, lambda x: x is not None and x != FLAGGED)
+                # print(f"Found {adjacent_numbered_count} numbered cells near {x},{y}: {coords} => {[cells[y][x] for y, x in coords]}")
 
-                    if adjacent_unknown_count == state:
-                        for target_x, target_y in coords:
-                            if not self.game.cell_flagged(target_x, target_y):
-                                self.game.toggle_flag(target_x, target_y)
-                                knowledge[target_y][target_x] = 1
+                # If we find no numbered cells nearby, we simply have to guess at the basic probability
+                # that there is a mine in this cell, independent of other cells.
+                if adjacent_numbered_count == 0:
+                    unknown_cell_penalty[(x, y)] = baseline_probability
+                else:
+                    # For each numbered cell, count the number of flags around it, and subtract
+                    # that from its total
+                    normalise = 0
+                    for target_x, target_y in coords:
+                        cell_number = cells[target_y][target_x]
+                        # print(f" ## {cell_number} ({target_y}, {target_x})")
+                        adjacent_flag_count, _ = MineSweeperAI._count_adjacent(cells, target_x, target_y, lambda x: x == FLAGGED)
+                        remaining_adjacent_mines = cell_number - adjacent_flag_count
 
-        # PASS 3
-        # After flagging, if something has only unknowns left, all are not bombs
-        for y in range(len(cells)):
-            for x in range(len(cells[y])):
+                        # print(f"# [{x}, {y}] -> {target_x}, {target_y} ({cell_number} - {adjacent_flag_count})")
+                        # If this number still has some knowledge remaining, allocate evenly between cells we don't know about
+                        # yet.
+                        if remaining_adjacent_mines == 0:
+                            continue
 
-                state = cells[y][x]
+                        # We must be one of these remaining adjacent unknowns
+                        remaining_unknown_count, _ = MineSweeperAI._count_adjacent(knowledge, target_x, target_y, lambda x: x == None)
+                        penalty = remaining_adjacent_mines / remaining_unknown_count
 
-                # Unknown cell: skip
-                if state is None or state == FLAGGED:
-                    continue
+                        # print(f"{remaining_adjacent_mines=}, {penalty=}")
+                        if (x, y) not in unknown_cell_penalty:
+                            unknown_cell_penalty[(x, y)] = 0 # FIXME: should this been baseline_probability?
+                        unknown_cell_penalty[(x, y)] += penalty
+                        normalise += 1
 
-                if state > 0:
-                    # Count adjacent unknowns
-                    adjacent_flag_count, _ = MineSweeperAI._count_adjacent(knowledge, x, y, lambda x: x == 1)
-                    adjacent_unknown_count, coords = MineSweeperAI._count_adjacent(knowledge, x, y, lambda x: x is None)
+                    # Divide penalty by the number of cells that contributed it, i.e. normalise
+                    unknown_cell_penalty[(x, y)] /= normalise
 
-                    if adjacent_flag_count == state and adjacent_unknown_count > 0:
-                        for target_x, target_y in coords:
-                            self.game.click(target_x, target_y)
-                            knowledge[target_y][target_x] = 0
 
+        if len(unknown_cell_penalty) == 0:
+            # Can't do anything!
+            return False
+
+        # print(f" -> {unknown_cell_penalty}")
+        # Find the lowest score and click it
+        cell_coord, penalty = sorted(unknown_cell_penalty.items(), key=lambda item: item[1])[0]
+        # print(f"Lowest penalty is #{cell_coord} with penalty={penalty}")
+        self.game.click(cell_coord[0], cell_coord[1])
+        return True
+
+
+    def move(self):
+
+        # Attempt deterministic actions
+        action = self._move_determinstic()
+
+        if not action:
+            #print("No action from deterministic solver, using heuristics.")
+            action = self._move_heuristic() or action
+        else:
+            pass
+            #print("Determinstic solution used.")
 
         # DEBUG
         # for row in knowledge:
--- ms.py
+++ ms.py
@@ -1,6 +1,6 @@
 
 
-
+import os, sys
 
 from board import Board, BOMB
 from game import MineSweeperGame
@@ -8,7 +8,7 @@ from render import render_interactive_board
 from ai import MineSweeperAI
 
 
-board = Board(1000, 1000, 0.1)
+board = Board(100, 100, 0.1)
 game  = MineSweeperGame(board)
 ai    = MineSweeperAI(game, board)
 
@@ -16,4 +16,33 @@ ai    = MineSweeperAI(game, board)
 
 render_interactive_board(board, game, ai)
 
+sys.exit(0)
 
+NUM_GAMES     = 100
+BOARD_WIDTH   = 100
+BOARD_HEIGHT  = 100
+BOARD_DENSITY = 0.2
+
+# 100 games
+log_wins = []
+log_moves = []
+for i in range(NUM_GAMES):
+
+    board = Board(BOARD_WIDTH, BOARD_HEIGHT, BOARD_DENSITY)
+    game  = MineSweeperGame(board)
+    ai    = MineSweeperAI(game, board)
+
+    while not game.finished:
+        ai.move()
+
+    log_wins.append(game.won)
+    log_moves.append(game.moves)
+    print(f"win? {game.won}, moves: {game.moves}")
+
+# Print summary
+print(f"Board with {BOARD_WIDTH}x{BOARD_HEIGHT} WxH, mine density={BOARD_DENSITY}")
+print(f"{sum(log_wins)} wins out of {len(log_wins)} games ({sum(log_wins) / len(log_wins) * 100:0.2f}%%)")
+print(f"{sum(log_moves) / len(log_moves):.2f} moves on average")
+without_lofc = [x for x in log_moves if x > 1]
+if len(without_lofc) > 0:
+    print(f"{sum(without_lofc) / len(without_lofc):.2f} discounting lose-on-first-click")
